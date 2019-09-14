e9845bb49d9e3db504884e7c80e0c3686d0067be
Fjallen <38442956+Fjallen@users.noreply.github.com>
2019-09-14 03:27:46 -0400

    Front end refactor

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Ranks from './Rank.jsx';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>
-          Get Good
-        </h1>
-        <p>
-          Which Game? <br></br>
-          <select name="game">
-            <option value="tft">Team Fighting Tactics</option>
-            <option value="lol">League of Legends</option>
-          </select>
-        </p>
-        <p>
-  <form action="">
-    In Game Name <br></br>
-    <input type='text' name="ign"></input>
-  </form>
-</p>
-<Ranks
-  property="d"
-/>
-    </div>
-      );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      rankins=[]
     }
-    
-    export default App;
+  };
+  getRankings(){
+    //Make Call Here Later
+    axios.get()
+    return 0;
+  }
+  render(){
+    return (
+      <div className="App">
+          <h1>
+            Get Good
+          </h1>
+          <p>
+            Which Game? <br></br>
+            <select name="game">
+              <option value="tft">Team Fighting Tactics</option>
+              <option value="lol">League of Legends</option>
+            </select>
+          </p>
+          <p>
+          <form action="">
+            In Game Name <br></br>
+            <input type='text' name="ign"></input>
+          </form>
+          </p>
+          <Ranks/>
+      </div>
+    )}
+}
+
+export default App;
