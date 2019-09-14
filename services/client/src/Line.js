0f622263c89c987303e9e79297ee78ec9c261d00
Vaid Saraswat <vaidsaraswat@gmail.com>
2019-09-14 18:32:26 -0400

    another commit

--- services/client/src/Line.js
+++ services/client/src/Line.js
@@ -26,7 +26,8 @@ var ChartData = {
       fill: false,
       backgroundColor: "rgba(218,83,79, .7)",
       borderColor: "rgba(218,83,79, .7)",
-      pointRadius: 0
+      pointRadius: 2,
+      showLine: true
     }
   ]
 };
@@ -38,15 +39,18 @@ export default class LineDemo extends Component {
     //this.props.config = line
   }
   init = (array) =>{
-    let a = this.props.data.pcot[0];
-    let b = this.props.data.pcot[1];
-    let c = this.props.data.pcot[2];
+    let a = this.props.data.popt[0];
+    let b = this.props.data.popt[1];
+    let c = this.props.data.popt[2];
     this.props.data.LPs.reverse();
     for(let i = 0; i < array.length; i++){
-      // var val;
-      // if (i==0){
-      // }
-      // var val = a * Math.log(b*i) + c
+       var val;
+       if (i===0){
+          val = a + c;
+       }
+       else{
+         val = a * Math.log(b*i) + c;
+       }
       ChartData.datasets[0].data.push({x:i,y:array[i]});
       ChartData.datasets[1].data.push({x:i,y:val});
     }
@@ -59,7 +63,7 @@ export default class LineDemo extends Component {
   render() {
     return (
       <div>
-        <h2>Line Example</h2>
+        <h2>Performance</h2>
         <Scatter ref={(reference)=> this.chartReference = reference} data={ChartData} />
       </div>
     );
