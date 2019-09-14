04a3faa38fb9647a02f7c6fda306042eb6b90b4b
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2019-09-14 01:28:26 -0400

    Updated HomeScreen

---
+++ services/client/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ranks from './Rank.jsx';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Get Good
+        </h1>
+        <p>
+          Which Game? <br></br>
+          <select name="game">
+            <option value="tft">Team Fighting Tactics</option>
+            <option value="lol">League of Legends</option>
+          </select>
+        </p>
+        <p>
+  <form action="">
+    In Game Name <br></br>
+    <input type='text' name="ign"></input>
+  </form>
+</p>
+<Ranks />
+    </div>
+      );
+    }
+    
+    export default App;
---
+++ services/client/src/Rank.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Ranks extends React.Component{
+    render(){
+        return (
+            <p>
+                jdaskjdasdkaj
+            </p>
+        )
+    }
+}
+export default Ranks;
