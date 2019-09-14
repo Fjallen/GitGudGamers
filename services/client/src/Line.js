f8ddff927df5823ed770e8c0067c5b230cb038cd
Fjallen <38442956+Fjallen@users.noreply.github.com>
2019-09-14 17:19:30 -0400

    Fixed frontend stuff

--- services/client/src/Line.js
+++ services/client/src/Line.js
@@ -1,38 +1,36 @@
 import React, {Component} from 'react';
 import { Scatter } from 'react-chartjs-2';
 
+var ChartData = {
+  labels: ['Scatter'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      backgroundColor: '#f0f8fc',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: []
+    }
+  ]
+};
+
+
 export default class LineDemo extends Component {
   constructor(props){
     super(props);
-    this.state ={
-      data:{
-        labels: ['Scatter'],
-        datasets: [
-          {
-            label: 'My First dataset',
-            fill: false,
-            backgroundColor: '#f0f8fc',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [
-              { x: 65, y: 75 },
-            ]
-          }
-        ]
-      }
-    }
     this.init = this.init.bind(this)
   }
   init = (array) =>{
-    var i;
     for(let i = 0; i < array.length; i++){
+      ChartData.datasets[0].data.push({x:i,y:array[i]})
     }
   };
 
@@ -44,7 +42,7 @@ export default class LineDemo extends Component {
     return (
       <div>
         <h2>Line Example</h2>
-        <Scatter ref="chart" data={this.state.data} />
+        <Scatter ref="chart" data={ChartData} />
       </div>
     );
   }
