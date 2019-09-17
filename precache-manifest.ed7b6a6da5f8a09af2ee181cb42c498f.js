c215857285045b64578211bce1bc541c1681545a
Fjallen <38442956+Fjallen@users.noreply.github.com>
2019-09-17 17:44:01 -0400

    Updates

---
+++ precache-manifest.ed7b6a6da5f8a09af2ee181cb42c498f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9473c9473f30ac57534da82124bf269",
+    "url": "/GitGudGamers/index.html"
+  },
+  {
+    "revision": "4215fb70c711887cdbb7",
+    "url": "/GitGudGamers/static/css/main.c8c044fe.chunk.css"
+  },
+  {
+    "revision": "bdb0e2393bf306dc491d",
+    "url": "/GitGudGamers/static/js/2.832b445c.chunk.js"
+  },
+  {
+    "revision": "4215fb70c711887cdbb7",
+    "url": "/GitGudGamers/static/js/main.56ed2fd5.chunk.js"
+  },
+  {
+    "revision": "0e5daa5fab6bafe6144c",
+    "url": "/GitGudGamers/static/js/runtime~main.46535e73.js"
+  },
+  {
+    "revision": "631ab28cfe16a2ab48c6556adfc57680",
+    "url": "/GitGudGamers/static/media/Arrow.631ab28c.gif"
+  }
+]);
